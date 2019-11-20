5eea3d390d7f5615b458f5542354b4dadbd47c76
Witek Podskarbi <witdev.front@gmail.com>
2019-11-20 21:29:17 +0100

    feat: created initial app view

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,25 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+// import About from '../view/About.vue';
+
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: Home,
   },
   {
     path: '/about',
-    name: 'about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component() {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue');
+      return import('../views/About.vue');
     },
   },
 ];
