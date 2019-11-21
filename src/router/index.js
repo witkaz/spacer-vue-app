f869cd1113b2544b7ea7c08571d31f4d1026cb98
Witek Podskarbi <witdev.front@gmail.com>
2019-11-21 20:14:56 +0100

    feat: connect app to NASA API, handelInput function

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
-// import About from '../view/About.vue';
+import Search from '../views/Search.vue';
+import About from '../views/About.vue';
 
 
 Vue.use(VueRouter);
@@ -9,18 +9,19 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'Search',
+    component: Search,
   },
   {
     path: '/about',
     name: 'About',
+    component: About,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component() {
-      return import('../views/About.vue');
-    },
+    // component() {
+    //   return import('../views/About.vue');
+    // },
   },
 ];
 
